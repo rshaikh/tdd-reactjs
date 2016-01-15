004ff36fbcf7a5b2cf4eca2dd63512157b101c05
rashid <rshaikh@equalexperts.com>
2016-01-15 20:37:04 +0530

    - Configure webpack jsx loader - Add Greeter react component

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   context: __dirname + "/src",
-  entry: "./app.js",
+  entry: "./app.jsx",
 
   output: {
     filename: "bundle.js",
     path: __dirname + "/src"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
-}
+};
